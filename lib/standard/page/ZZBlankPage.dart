33fd578ff0d037590eb58ca767380593db5869cc
Jeff <fujie@55haitao.com>
2024-08-28 09:53:30 +0800

    add blankpage

---
+++ lib/standard/page/ZZBlankPage.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable, file_names
+import 'package:flutter/material.dart';
+
+import 'package:zzkit_flutter/util/core/ZZConst.dart';
+
+class ZZBlankPage extends StatelessWidget {
+  Color? color;
+  ZZBlankPage({
+    super.key,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: zzScreenWidth,
+      height: zzScreenHeight,
+      color: color ?? Colors.white,
+    );
+  }
+}
